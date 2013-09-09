adf49e74a1e8446d3f68359f6ec5f37a52c6ef25
Nedzad Kulelija <nedzadk@gmail.com>
2013-09-09 10:15:31 +0200

    Added headers to urllib2 request

--- html_sniffer.py
+++ html_sniffer.py
@@ -1,6 +1,9 @@
 import urllib2,sys,re
 
-f = urllib2.urlopen(sys.argv[1]) #Load URL from first command line argument
+heads = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3','Accept-Encoding': 'none','Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
+
+request = urllib2.Request(sys.argv[1],headers=heads) #Request to site with headers
+f = urllib2.urlopen(request)
 html = f.read() #Read html from url
 html = re.sub('<[^<]+?>','',html) #Remove HTML tags
 html = " ".join(html.split()) #Remove multiple spaces (better rading)
