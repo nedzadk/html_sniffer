1e1eb01c42bc3bb477e4e58dfdf8f092bd0bbaab
Nedzad Kulelija <nedzadk@gmail.com>
2013-09-10 12:34:36 +0200

    Ninja version

--- html_sniffer.py
+++ html_sniffer.py
@@ -2,6 +2,31 @@ import urllib2
 import sys
 import re
 
+
+def strip_string(text, starting_offset):
+    x = starting_offset
+    found = False
+    new_text = ""
+    while (found is not True):
+        x -= 1
+        if text[x] != '>':
+            new_text = new_text + text[x]
+        else:
+            found = True
+
+    x = starting_offset
+    found = False
+    new_text2 = ""
+    while (found is not True):
+        x += 1
+        if text[x] != '<':
+            new_text2 = new_text2 + text[x]
+        else:
+            found = True
+    # Revers first part of the result when returning result
+    return new_text[::-1] + text[starting_offset] + new_text2
+
+
 heads = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64',
          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
          'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -13,33 +38,44 @@ request = urllib2.Request(sys.argv[1], headers=heads)
 f = urllib2.urlopen(request)
 # read html from url
 html = f.read()
-# remove html tags
-html = re.sub('<[^<]+?>', '', html)
 # remove multiple spaces (for better looking results)
 html = " ".join(html.split())
 
 # convert to lower case for case insensitive search
 lower_string = html.lower()
-result = lower_string.find(sys.argv[2].lower())
-cmd_args = len(sys.argv)
+result = 0
+print "String found!!!!"
+print "Part of the string with offsets defined"
+print "============= BEGIN ==================="
+# Loop through html for multpiple search
+while result < len(html):
 
-if cmd_args > 3:
-    # first offset argument
-    offset_before = int(sys.argv[3])
-else:
-    offset_before = 0
+    result = lower_string.find(sys.argv[2].lower(), result)
+    cmd_args = len(sys.argv)
 
-if cmd_args > 4:
-    # second offset argument
-    offset_after = int(sys.argv[4])
-else:
-    offset_after = 100
+    if cmd_args > 3:
+        # first offset argument
+        offset_before = int(sys.argv[3])
+    else:
+        offset_before = 0
 
-if result != -1:
-    print "String found!!!!"
-    print "Part of the string with offsets defined"
-    print "======================================="
-    print html[result - offset_before:result + offset_after]
-    print "======================================="
-else:
-    print "String not found (info: search is not case sensitive)"
+    if cmd_args > 4:
+        # second offset argument
+        offset_after = int(sys.argv[4])
+    else:
+        offset_after = 100
+
+    if result != -1:
+        # Uncomment for dirty result (debug)
+        # print html[result - offset_before:result + offset_after]
+        # Calling strip_string function that cleans result and do some other
+        # cleaning (results that contains only html tags
+        final_text = re.sub('<[^<]+?>', '', strip_string(html, result))
+        if len(final_text) >= len(sys.argv[2]):
+            print final_text
+            print "--------------------------------------"
+    else:
+        print "=============== END =================="
+        break
+
+    result += len(sys.argv[2])
