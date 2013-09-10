c57fc0b6e283ed922cb645513e51ef3984b0b89b
Nedzad Kulelija <nedzadk@gmail.com>
2013-09-10 11:20:31 +0200

    Code cleanup

--- html_sniffer.py
+++ html_sniffer.py
@@ -1,37 +1,43 @@
 import urllib2,sys,re
 
-heads = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+heads = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64',
          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
          'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
          'Accept-Encoding': 'none',
          'Accept-Language': 'en-US,en;q=0.8',
          'Connection': 'keep-alive'}
 
-request = urllib2.Request(sys.argv[1],headers=heads) #Request to site with headers
+request = urllib2.Request(sys.argv[1], headers=heads)
 f = urllib2.urlopen(request)
-html = f.read() #Read html from url
-html = re.sub('<[^<]+?>','',html) #Remove HTML tags
-html = " ".join(html.split()) #Remove multiple spaces (better rading)
+# read html from url
+html = f.read()
+# remove html tags
+html = re.sub('<[^<]+?>', '', html)
+# remove multiple spaces (for better looking results)
+html = " ".join(html.split())
 
-lower_string = html.lower() # Convert to lower case for case insensitiv search
-result = lower_string.find(sys.argv[2].lower()) #Find string index (if string exist)
+# convert to lower case for case insensitive search
+lower_string = html.lower()
+result = lower_string.find(sys.argv[2].lower())
 cmd_args = len(sys.argv)
 
-if cmd_args>3:
-    offset_before = int(sys.argv[3]) #First offset argument
+if cmd_args > 3:
+    # first offset argument
+    offset_before = int(sys.argv[3])
 else:
     offset_before = 0
 
-if cmd_args>4:
-    offset_after = int(sys.argv[4])  #Second offset argument
+if cmd_args > 4:
+    # second offset argument
+    offset_after = int(sys.argv[4])
 else:
     offset_after = 100
 
-if result<>-1:
+if result != -1:
     print "String found!!!!"
     print "Part of the string with offsets defined"
     print "======================================="
-    print html[result-offset_before:result+offset_after] #Show string based on defined offsets
+    print html[result - offset_before:result + offset_after]
     print "======================================="
 else:
     print "String not found (info: search is not case sensitive)"
